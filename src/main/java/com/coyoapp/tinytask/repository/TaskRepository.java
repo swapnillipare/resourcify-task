ef9a4d10a1c43bfdb5923439ef61c4291adcebe6
swapnil <swapnil@suntelglobal.net>
2024-10-26 14:13:37 +0200

    Sorting with null at last with custom query

--- src/main/java/com/coyoapp/tinytask/repository/TaskRepository.java
+++ src/main/java/com/coyoapp/tinytask/repository/TaskRepository.java
@@ -2,9 +2,11 @@ package com.coyoapp.tinytask.repository;
 
 import com.coyoapp.tinytask.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, String> {
+  @Query("SELECT t FROM Task t ORDER BY CASE WHEN t.dueDate IS NULL THEN 1 ELSE 0 END, t.dueDate ASC")
   List<Task> findAllByOrderByDueDateAscDueDateIsNull(); // Sort by due date
 }
