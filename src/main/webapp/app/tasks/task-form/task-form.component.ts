316c84a62d05232910151b5d04a2c8c39f76146d
swapnil <swapnil@suntelglobal.net>
2024-10-26 11:42:35 +0000

    Date mudule added to tasks

--- src/main/webapp/app/tasks/task-form/task-form.component.ts
+++ src/main/webapp/app/tasks/task-form/task-form.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Inject, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { DatePipe } from '@angular/common';
 import { Task } from '../task';
 import { TaskService } from '../task.service';
 
@@ -11,6 +11,7 @@ import { TaskService } from '../task.service';
   selector: 'tiny-task-form',
   templateUrl: './task-form.component.html',
   styleUrls: ['./task-form.component.scss'],
+  providers: [DatePipe],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TaskFormComponent {
@@ -18,13 +19,21 @@ export class TaskFormComponent {
   @Output() created: EventEmitter<Task> = new EventEmitter();
 
   taskForm: FormGroup = new FormGroup({
-    name: new FormControl('', Validators.required)
+    name: new FormControl('', Validators.required),
+    dueDate: new FormControl(null)
   });
 
-  constructor(@Inject('TaskService') private taskService: TaskService) { }
+  constructor(@Inject('TaskService') private taskService: TaskService,private datePipe: DatePipe) { }
 
   onSubmit(): void {
-    this.taskService.create(this.taskForm.value.name).subscribe(task => {
+    const formValue = this.taskForm.value;
+    const formattedDueDate = this.datePipe.transform(formValue.dueDate, "yyyy-MM-dd'T'HH:mm:ss.SSS");
+
+    const taskData = {
+      name: formValue.name,
+      dueDate: formattedDueDate // Send the formatted date
+    };
+    this.taskService.create(taskData.name,taskData.dueDate).subscribe(task => {
       this.created.emit(task);
       this.taskForm.reset();
     });
